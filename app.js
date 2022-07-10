1f2e70c09b50dcbb0744607aaeb92cd40f229581
vitalymuravyev <vitalymuravyev@gmail.com>
2022-07-10 22:41:17 +0300

    Fix linters warnings

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const express = require('express');
 const PORT = 3000;
 const app = express();
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.resolve(__dirname, "./index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './index.html'));
 });
 
 app.listen(PORT, () => console.log(`App is ready on port ${PORT}`));
