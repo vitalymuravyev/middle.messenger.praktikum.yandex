cfb5843f2343e4f5c28624f6ed35f478f3cb46ad
vitalymuravyev <vitalymuravyev@gmail.com>
2022-08-13 12:45:41 +0300

    fix path at app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist'));
+  res.sendFile(path.resolve(path.join(__dirname, 'dist'), './index.html'));
 });
 
 app.listen(PORT, () => console.log(`App is ready on port ${PORT}`));
